cde26c0a581612099b1c8c4de43ca8ffc44243f4
Jonathan Heugh <68595133+Jontas77@users.noreply.github.com>
2021-09-13 23:03:38 +0200

    Added login and dashboard routes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Home.css";
-import logo from "./logo.svg";
+
 
 export function Home() {
 	const [message, setMessage] = useState("Loading...");
@@ -26,16 +26,12 @@ export function Home() {
 	return (
 		<main role="main">
 			<div>
-				<img
-					className="logo"
-					data-qa="logo"
-					src={logo}
-					alt="Just the React logo"
-				/>
 				<h1 className="message" data-qa="message">
 					{message}
 				</h1>
-				<Link to="/about/this/site">About</Link>
+				<select name="dropdown" id="dropdown">
+					<Link to="/student/login"><option value="student">Student</option></Link>
+				</select>
 			</div>
 		</main>
 	);
