2e73a5403a302fd37f3b54f1806899757a30860c
Lawrence <lawrencebaatjies47@gmail.com>
2021-09-14 10:30:10 +0200

    Update db.sql

--- server/db.sql
+++ server/db.sql
@@ -19,7 +19,7 @@ CREATE TABLE students (
 
 id            		 SERIAL PRIMARY KEY,  
 student_id        	 INT NOT NULL,
-student_name 		 VARCHAR(255) NOT NULL,
+student_name 		     VARCHAR(255) NOT NULL,
 cell_number          VARCHAR(15)  NOT NULL,
 email                VARCHAR(255) NOT NULL,
 password             VARCHAR(255) NOT NULL UNIQUE
@@ -34,8 +34,8 @@ CREATE TABLE projects (
 id            	SERIAL PRIMARY KEY,  
 student_id      INT NOT NULL,
 advisor_id      INT NOT NULL,
-project_name    VARCHAR NOT NULL,
-project_status  VARCHAR NOT NULL
+project_name    VARCHAR(255) NOT NULL,
+project_status  VARCHAR(255) NOT NULL
 
 );
 
@@ -44,10 +44,10 @@ SELECT * FROM projects;
 CREATE TABLE project_proposal (
 
 id            			SERIAL PRIMARY KEY,  
-project_name_id			VARCHAR(255) NOT NULL,
-problem_statement_id    	VARCHAR(255) NOT NULL,
-proposed_action_id  	VARCHAR(500) NOT NULL,
-project_status_id   	VARCHAR(255) NOT NULL
+project_name_id			    VARCHAR(255) NOT NULL,
+problem_statement_id    VARCHAR(255) NOT NULL,
+proposed_action_id  	  VARCHAR(500) NOT NULL,
+project_status_id      	VARCHAR(255) NOT NULL
 );
 
 SELECT * FROM project_proposal;
