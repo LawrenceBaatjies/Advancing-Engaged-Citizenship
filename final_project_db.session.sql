a544a3b7aed539fdd5eb0670af307802617a86aa
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-09-14 22:43:06 +0200

    Update final_project_db.session.sql

--- final_project_db.session.sql
+++ final_project_db.session.sql
@@ -1,19 +1,33 @@
-CREATE  DATABASE advancing_engaged_citizenship;
+CREATE 
+    DATABASE 
+        advancing_engaged_citizenship;
 
+CREATE TABLE admin (
+id             SERIAL PRIMARY KEY,
+admin_id             INT NOT NULL,
+admin_name           VARCHAR(255) NOT NULL,
+cell_number          VARCHAR(15)  NOT NULL,
+email                VARCHAR(255) NOT NULL,
+password             VARCHAR(255) NOT NULL UNIQUE
 
+);
 
-CREATE  TABLE  advisors(
+SELECT *
+ FROM admin;
+
+CREATE  TABLE  mentor(
 
 id            	SERIAL PRIMARY KEY,              
-advisors_id     INT NOT NULL,
-advisor_name   	VARCHAR(255) NOT NULL UNIQUE,
+mentor_id       INT NOT NULL,
+mentor_name   	VARCHAR(255) NOT NULL UNIQUE,
 cell_number   	VARCHAR(15) NOT NULL,
 email          	VARCHAR(255) NOT NULL,
 password       	VARCHAR(255) NOT NULL UNIQUE
 
 );
 
-SELECT * FROM advisors;
+SELECT * 
+    FROM mentor;
 
 
 CREATE TABLE students (
@@ -27,28 +41,31 @@ password             VARCHAR(255) NOT NULL UNIQUE
 
 );
 
-SELECT * FROM students;
+SELECT * 
+    FROM students;
 
 
 CREATE TABLE projects (
 
-id            	SERIAL PRIMARY KEY,  
-student_id      INT NOT NULL,
-advisor_id      INT NOT NULL,
-project_name    VARCHAR NOT NULL,
-project_status  VARCHAR NOT NULL
+id            	    SERIAL PRIMARY KEY,  
+student_id          INT NOT NULL,
+advisor_id          INT NOT NULL,
+project_name        VARCHAR(255) NOT NULL,
+project_status      VARCHAR(255) NOT NULL
 
 );
 
-SELECT * FROM projects;
+SELECT * 
+    FROM projects;
 
 CREATE TABLE project_proposal (
 
-id            			SERIAL PRIMARY KEY,  
-project_name_id			VARCHAR(255) NOT NULL,
-problem_statement_id    	VARCHAR(255) NOT NULL,
-proposed_action_id  	VARCHAR(500) NOT NULL,
-project_status_id   	VARCHAR(255) NOT NULL
+id            			    SERIAL PRIMARY KEY,  
+project_name_id			    VARCHAR(255) NOT NULL,
+problem_statement_id        VARCHAR(255) NOT NULL,
+proposed_action_id  	    VARCHAR(500) NOT NULL,
+project_status_id      	    VARCHAR(255) NOT NULL
 );
 
-SELECT * FROM project_proposal;
+SELECT * 
+    FROM project_proposal;
