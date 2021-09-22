5988719644fb1e372a4139261d765ef43e66e8be
Jontas77 <mrjbheugh@gmail.com>
2021-09-22 23:09:21 +0200

    created auth routes in backend

--- server/db.js
+++ server/db.js
@@ -1,7 +1,9 @@
 import { Pool } from "pg";
 require("dotenv").config();
 
-const dbUrl = process.env.DATABASE_URL ||  `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`;
+const dbUrl =
+	process.env.DATABASE_URL ||
+	`postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`;
 
 const pool = new Pool({
 	connectionString: dbUrl,
