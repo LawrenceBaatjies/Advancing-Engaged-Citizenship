9fa18885f02b788ed3754fc4b7275645596cbd44
Jontas77 <mrjbheugh@gmail.com>
2021-09-25 01:24:40 +0200

    Added header component to dash

--- client/src/pages/dashboards/StudentDashboard.js
+++ client/src/pages/dashboards/StudentDashboard.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
 
 const StudentDashboard = ({ setAuth }) => {
 	return (
+		<>
+		<Header />
 		<div>
 			<h1>WELCOME STUDENT</h1>
 			<h2>DASHBOARD</h2>
 			<button onClick={() => setAuth(false)}>Log out</button>
 		</div>
+		</>
 	);
 };
 
---
+++ client/src/pages/dashboards/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+	return <header style={{ backgroundColor: "red" }}>
+        <p>Header</p>
+    </header>;
+};
+
+export default Header;
